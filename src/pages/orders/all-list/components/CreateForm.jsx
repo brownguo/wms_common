217e492571da528f920ab995337698b488237998
brownguo <yexuan.guo@codedance.cn>
2020-03-19 17:04:33 +0800

    created orders ui

--- src/pages/orders/all-list/components/CreateForm.jsx
+++ src/pages/orders/all-list/components/CreateForm.jsx
@@ -16,7 +16,7 @@ const CreateForm = props => {
   return (
     <Modal
       destroyOnClose
-      title="新建规则"
+      title="新建订单"
       visible={modalVisible}
       onOk={okHandle}
       onCancel={() => onCancel()}
@@ -29,18 +29,64 @@ const CreateForm = props => {
           wrapperCol={{
             span: 15,
           }}
-          label="描述"
-          name="desc"
-          rules={[
-            {
-              required: true,
-              message: '请输入至少五个字符的规则描述！',
-              min: 5,
-            },
-          ]}
+          label="姓名"
+          name="username"
         >
           <Input placeholder="请输入" />
         </FormItem>
+
+        <FormItem
+            labelCol={{
+              span: 5,
+            }}
+            wrapperCol={{
+              span: 15,
+            }}
+            label="手机号"
+            name="mobile_number"
+        >
+          <Input placeholder="请输入" />
+        </FormItem>
+
+        <FormItem
+            labelCol={{
+              span: 5,
+            }}
+            wrapperCol={{
+              span: 15,
+            }}
+            label="金额"
+            name="money"
+        >
+          <Input placeholder="请输入" />
+        </FormItem>
+
+        <FormItem
+            labelCol={{
+              span: 5,
+            }}
+            wrapperCol={{
+              span: 15,
+            }}
+            label="地址"
+            name="address"
+        >
+          <Input.TextArea />
+        </FormItem>
+
+        <FormItem
+            labelCol={{
+              span: 5,
+            }}
+            wrapperCol={{
+              span: 15,
+            }}
+            label="备注"
+            name="desc"
+        >
+          <Input.TextArea />
+        </FormItem>
+
       </Form>
     </Modal>
   );
